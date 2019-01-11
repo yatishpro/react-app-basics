e285f8b2ddbbd79362946c772ed67be1c98dff38
yatishpro <yatishpro@gmail.com>
2019-01-11 16:13:06 +0530

    Updates

---
+++ precache-manifest.50eeda95a22d14ada66fbfdabc44f138.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb3a5ceb0e12940a9242",
+    "url": "/react-app-basics/static/js/runtime~main.bb3a5ceb.js"
+  },
+  {
+    "revision": "b75550430b6b37d0b8b1",
+    "url": "/react-app-basics/static/js/main.b7555043.chunk.js"
+  },
+  {
+    "revision": "0eba81d3f81d2ac1c910",
+    "url": "/react-app-basics/static/js/1.0eba81d3.chunk.js"
+  },
+  {
+    "revision": "b75550430b6b37d0b8b1",
+    "url": "/react-app-basics/static/css/main.0ad3a041.chunk.css"
+  },
+  {
+    "revision": "6922bb018bc514fe2ac7d07a1bb1aad7",
+    "url": "/react-app-basics/index.html"
+  }
+];
